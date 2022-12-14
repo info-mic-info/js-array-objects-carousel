cb9592d6f3fb9ae16d596821be6aca992a3441c4
info-mic-info <jobsocial94@gmail.com>
2022-12-14 16:44:21 +0100

    Bug

--- js/script.js
+++ js/script.js
@@ -1,3 +1,157 @@
-// Milestone 0: Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
-// Milestone 1: Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.   Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
-// Milestone 2: Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
+//Creo array immagini
+
+const imagesArray = [
+    {
+        image: "01.webp",
+        title: `spiderman`,
+        description: `description1`
+
+    },
+    {
+        image: "02.webp",
+        title: `prova2`,
+        description: `description2`
+
+    },
+    {
+        image: "03.webp",
+        title: `prova3`,
+        description: `description3`
+
+    },
+    {
+        image: "04.webp",
+        title: `prova4`,
+        description: `description4`
+
+    },
+    {
+        image: "05.webp",
+        title: `prova5`,
+        description: `description5`
+
+    }
+]
+
+function nextSlide() {
+    if (itemActive < imagesArray.length - 1) {
+
+        items[itemActive].classList.remove(`active`);
+        circles[itemActive].classList.remove('active');
+
+        itemActive += 1
+        // itemActive ++
+
+        circles[itemActive].classList.add('active');
+        items[itemActive].classList.add(`active`)
+    }
+
+    else {
+        items[itemActive].classList.remove(`active`);
+        circles[itemActive].classList.remove('active');
+
+        itemActive = 0
+        circles[itemActive].classList.add('active');
+        items[itemActive].classList.add(`active`)
+
+    }
+}
+
+// QUESTO
+
+// _______________________________________________________________
+
+//Creiamo dinamicamente i div con le immagini del carosello
+let itemsContent = '';
+
+imagesArray.forEach((elem) => {
+    itemsContent += <div class="item">
+    <img src="${elem.image}">
+    <div class="desc text-white">
+    <h2>${elem.title}</h2>
+    <p>${elem.description}</p>
+    </div>
+    </div>
+});
+
+// for (let i = 0; i < imagesArray.length; i++) {
+//     itemsContent += `<div class="item">
+//         <img src="./img/${imagesArray[i]}">
+//     </div>`
+// }
+// _____________________________________________________________________
+//inseriamo le immagini nel div che le deve contenere
+const itemsSlider = document.querySelector('.item-slider');
+itemsSlider.innerHTML += itemsContent;
+
+//Prendiamo la prima immagine dell'array e la rendiamo attiva
+
+//const items = document.querySelector('.item'); //ALTERNATIVA
+
+const items = document.getElementsByClassName('item');
+let itemActive = 0;
+
+items[itemActive].classList.add('active');
+
+//rendo attivo anche il primo cerchio di navigazione
+
+const circles = document.getElementsByClassName('circle');
+
+circles[itemActive].classList.add('active');
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+
+// ********************* ESERCIZIO*************************
+
+next.addEventListener('click', nextSlide())
+    //verifico l'elemento attivo (itemActive)
+    //incremento il suo valore di 1
+    //aggiungere la class active al nuovo elemento dell'array items e la vado a rimuovere da quello precedente
+    //stessa cosa per i cerchi
+    //verifico l'elemento attivo (itemActive)
+
+
+
+    ;
+
+prev.addEventListener('click', function () {
+    //verifico l'elemento attivo (itemActive)
+    //decremento il suo valore di 1
+    //aggiungere la class active al nuovo elemento dell'array items e la vado a rimuovere da quello precedente
+    //stessa cosa per i cerchi
+
+    if (itemActive > 0) {
+
+        items[itemActive].classList.remove(`active`);
+        circles[itemActive].classList.remove('active');
+
+
+        itemActive -= 1
+        circles[itemActive].classList.add('active');
+        items[itemActive].classList.add(`active`)
+    }
+
+    else {
+        items[itemActive].classList.remove(`active`);
+        circles[itemActive].classList.remove('active');
+
+        itemActive = items.length - 1
+        circles[itemActive].classList.add('active');
+        items[itemActive].classList.add(`active`)
+
+    }
+
+})
+
+
+let myInterval = setInterval(nextSlide, 2000)
+
+document.getElementById(`play`).addEventListener(`click`, function () {
+    myInterval = setInterval(nextSlide, 2000)
+})
+
+document.getElementById(`pause`).addEventListener(`click`, function () {
+    clearInterval(myInterval)
+})
